ab9d09723a5ce61d9e71649c91128fa50faa7b2c
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2017-10-28 15:34:55 +0100

    add verifications to models

---
+++ app/controllers/pubs_controller.rb
@@ -0,0 +1,21 @@
+class PubsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home, :index, :search, :show, :new, :create]
+  after_action :verify_authorized, except: [:home]
+  def home
+    @cities = City.all
+  end
+
+  def index
+    @pubs = policy_scope(Pub).order(created_at: :desc)
+  end
+
+  def new
+    @pub = Pub.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- app/models/pub.rb
+++ app/models/pub.rb
@@ -1,5 +1,5 @@
 class Pub < ApplicationRecord
-  has_many :drinks
+  has_many :drinks, dependent: :destroy
   belongs_to :city
   has_many :users
   validates :name, :opening_time, :closing_time, :food, presence: true
