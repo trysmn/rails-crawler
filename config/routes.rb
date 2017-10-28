d7b37d2da628d1a95754fb9cf228c15140ff66b5
VIMLAN GANESAN <gvimlan95@gmail.com>
2017-10-28 22:05:49 +0100

    front page and second page map added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pubs#home'
 
+
   resources :pubs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
